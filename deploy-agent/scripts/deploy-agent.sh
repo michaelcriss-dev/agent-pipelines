d6c84280302fb2adbd83142a2a5fd2f1d1f24ddf
root <root@AUTOP-drdT6bAs8.babel-si.local>
2025-10-16 16:58:01 +0200

    echos

--- deploy-agent/scripts/deploy-agent.sh
+++ deploy-agent/scripts/deploy-agent.sh
@@ -5,7 +5,8 @@ IFS=$'\n\t'
 DB_RG="my-db-rg"
 MYSQL_SERVER="my-mysql-serversad2eda3ead"
 ADMIN_USER="azureuser"
-MYSQL_VERSION="8.0.23"
+ADMIN_USER_FULL="${ADMIN_USER}@${MYSQL_SERVER}"
+MYSQL_VERSION="8.0.21"
 MYSQL_DB="stomology_dep"
 DB_HOST="my-mysql-serversad2eda3ead.mysql.database.azure.com"  
 
@@ -18,22 +19,18 @@ WEB_APP_NAME="my-web-app-3434323423"
 DOCKER_IMAGE="michael2209/fullstack-app:v1.0"
 LOCATION="Canada Central"
 
+echo "Iniciando script de despliegue"
 
-# Azure Login
-
+echo "Autenticando en Azure"
 az login --service-principal \
     -u "$ARM_CLIENT_ID" \
     -p "$ARM_CLIENT_SECRET" \
     --tenant "$ARM_TENANT_ID"
 
-
-# Crear Resource Group para DB
-
+echo "Creando Resource Group para DB"
 az group create --name "${DB_RG}" --location "${LOCATION}"
 
-
-# Crear MySQL Flexible Server
-
+echo "Creando MySQL Flexible Server"
 az mysql flexible-server create \
   --name "${MYSQL_SERVER}" \
   --resource-group "${DB_RG}" \
@@ -51,29 +48,24 @@ az mysql flexible-server firewall-rule create \
   --start-ip-address 0.0.0.0 \
   --end-ip-address 0.0.0.0
 
-# Clonar o actualizar repositorio
-git clone $REPO_URL
+echo "Clonando repositorio"
+git clone "$REPO_URL"
+cd fullstack-app/SQL
 
-
-# Importar base de datos si no existe
-
+echo "Importando base de datos"
 export MYSQL_PWD="$ADMIN_PASSWORD"
 if ! mysql -h "${DB_HOST}" -u "${ADMIN_USER_FULL}" --silent --skip-column-names -e "USE ${MYSQL_DB};" 2>/dev/null; then
-    echo "Importing DB..."
     mysql -h "${DB_HOST}" -u "${ADMIN_USER_FULL}" -e "CREATE DATABASE ${MYSQL_DB};"
     mysql -h "${DB_HOST}" -u "${ADMIN_USER_FULL}" "${MYSQL_DB}" < stomology_dep.sql
 else
-    echo "Database already exists..."
+    echo "Database already exists"
 fi
 unset MYSQL_PWD
 
-
-# Crear Resource Group para Web App
-
+echo "Creando Resource Group para App"
 az group create --name "${APP_RG}" --location "${LOCATION}"
 
-
-# Crear App Service Plan
+echo "Creando App Service Plan"
 az appservice plan create \
   --name "${PLAN_NAME}" \
   --resource-group "${APP_RG}" \
@@ -81,22 +73,20 @@ az appservice plan create \
   --is-linux \
   --sku "${SKU}"
 
-
-# Crear Web App y desplegar imagen Docker
+echo "Creando Web App y desplegando imagen Docker"
 az webapp create \
   --resource-group "${APP_RG}" \
   --plan "${PLAN_NAME}" \
   --name "${WEB_APP_NAME}" \
   --deployment-container-image-name "${DOCKER_IMAGE}"
 
-
-# Configurar variables de entorno de la Web App
+echo "Configurando variables de entorno"
 az webapp config appsettings set \
   --resource-group "${APP_RG}" \
   --name "${WEB_APP_NAME}" \
   --settings \
     DB_HOST="${DB_HOST}" \
-    DB_USER="${ADMIN_USER}" \
+    DB_USER="${ADMIN_USER_FULL}" \
     DB_PASSWORD="${ADMIN_PASSWORD}"
 
-echo "Deployment complete!"
+echo "Despliegue completado"
